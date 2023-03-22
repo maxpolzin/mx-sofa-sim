a876ee6b882e3e30330d5b5c49e3bc07bea36ab0
Max Polzin <max.polzin@epfl.ch>
2023-03-22 12:48:19 +0100

    Add new scene.

--- mx_new/mx_sim.py
+++ mx_new/mx_sim.py
@@ -14,13 +14,12 @@ class Noodlebot(Sofa.Prefab):
 
     """Creates an object composed of an elastic material."""
     prefabParameters = [
-        {'name': 'volumeMeshFileName', 'type': 'string', 'help': 'Path to volume mesh file', 'default': ''},
         {'name': 'rotation', 'type': 'Vec3d', 'help': 'Rotation', 'default': [0.0, 0.0, 0.0]},
         {'name': 'translation', 'type': 'Vec3d', 'help': 'Translation', 'default': [0.0, 0.0, 0.0]},
         {'name': 'scale', 'type': 'Vec3d', 'help': 'Scale 3d', 'default': [1.0, 1.0, 1.0]},
         {'name': 'poissonRatio', 'type': 'double', 'help': 'Poisson ratio', 'default': 0.3},
         {'name': 'youngModulus', 'type': 'double', 'help': "Young's modulus", 'default': 18000},
-        {'name': 'totalMass', 'type': 'double', 'help': 'Total mass', 'default': 1.0}]
+        {'name': 'totalMass', 'type': 'double', 'help': 'Total mass', 'default': 2.0}]
 
 
 
@@ -34,7 +33,7 @@ class Noodlebot(Sofa.Prefab):
         self.loader = self.addObject('MeshOBJLoader', name='loader', triangulate=True, filename='mesh/Body8_lowres_mm_2.obj', rotation=list(self.rotation.value), translation=list(self.translation.value),
                                          scale3d=list(self.scale.value))
         self.dofs = self.addObject('MechanicalObject', template='Vec3d', name='dofs')
-        self.mass = self.addObject('DiagonalMass', totalMass=2.0, name='mass')
+        self.mass = self.addObject('DiagonalMass', totalMass=self.totalMass.value, name='mass')
 
         self.container = self.addObject('SparseGridRamificationTopology', n=[10, 10, 10], src=self.loader.getLinkPath(), name='container')
 
@@ -42,7 +41,6 @@ class Noodlebot(Sofa.Prefab):
                                             method='large', name='forcefield',
                                             poissonRatio=self.poissonRatio.value, youngModulus=self.youngModulus.value)
 
-        # self.constraingcorrection = self.addObject('GenericConstraintCorrection', name="constraingcorrection")
         self.constraingcorrection = self.addObject('LinearSolverConstraintCorrection', name='constraingcorrection')
 
 
