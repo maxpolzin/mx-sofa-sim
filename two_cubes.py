d7a0bf81e088e6c1dc9d66ce87f13164af96b1c4
Max Polzin <polzin.max@gmail.com>
2023-04-19 16:20:01 +0200

    Big success. Works.

--- two_cubes.py
+++ two_cubes.py
@@ -9,16 +9,52 @@ class CubeController(Sofa.Core.Controller):
     def __init__(self, *a, **kw):
         Sofa.Core.Controller.__init__(self, *a, **kw)
         self.node = kw["node"]
+        self.name = "CubeController"
         return
 
     def reset(self):
         rotation = 0
-        self.node.tentacle.actuator.cable.findData('value').value = rotation
+        self.node.Simulation.CubeBot.findData('angleIn').value = rotation
 
     def onAnimateBeginEvent(self, event):
-        rotation = self.node.tentacle.actuator.cable.findData('value').value[0]
+        rotation = self.node.Simulation.CubeBot.findData('angleIn').value
         rotation += 0.05
-        self.node.tentacle.actuator.cable.findData('value').value = [rotation]
+        self.node.Simulation.CubeBot.findData('angleIn').value = rotation
+
+
+class CubeBot(Sofa.Prefab):
+
+    prefabParameters = []
+    prefabData = [
+        {'name': 'angleIn', 'help': 'angle of rotation (in radians)', 'type': 'float', 'default': 0},
+    ]
+
+    def __init__(self, *args, **kwargs):
+        Sofa.Prefab.__init__(self, *args, **kwargs)
+
+    def init(self):
+
+        cube_red = Cube(self, totalMass=500, name="Red", color=[1., 0., 0., 1.], translation=[1.25, 5.0, 0.0], rotation=[0.0, 90.0, 0.0], uniformScale=1.0, isAStaticObject=False)    
+
+        angle = cube_red.addChild('Articulation')
+        angle.addObject('MechanicalObject', name='dofs', template='Vec1', position=[[self.getData('angleIn')]], rest_position=self.getData('angleIn').getLinkPath())
+        angle.addObject('RestShapeSpringsForceField', points=0, stiffness=1e7)
+
+
+        articulationCenter = angle.addChild('ArticulationCenter')
+        articulationCenter.addObject('ArticulationCenter', parentIndex=0, childIndex=0, posOnParent=[0, 0., 2.8], posOnChild=[0., 0., 0.])
+        
+        articulation = articulationCenter.addChild('Articulations')
+        articulation.addObject('Articulation', translation=False, rotation=True, rotationAxis=[1, 0, 0],articulationIndex=0)
+        
+        angle.addObject('ArticulatedHierarchyContainer', printLog=False)
+
+
+        cube_blue = Cube(angle, name="Blue", totalMass=500, color=[0., 0., 1., 1.], translation=[-0.75, 5.5, 0.0], rotation=[45.0, 0.0, 20.0], uniformScale=0.8, isAStaticObject=False)
+
+        cube_blue.addObject('ArticulatedSystemMapping', input1="@../dofs", input2="@../../mstate", output="@./")
+
+
 
 
 def createScene(rootNode):
@@ -58,35 +94,10 @@ def createScene(rootNode):
 
     scene.Simulation.addObject('GenericConstraintCorrection')
 
+    cubeBot = CubeBot()
+    scene.Simulation.addChild(cubeBot)
 
-    cube_red = Cube(scene.Simulation, totalMass=500, name="Red", color=[1., 0., 0., 1.], translation=[1.25, 5.0, 0.0], rotation=[0.0, 90.0, 0.0], uniformScale=1.0, isAStaticObject=False)    
-
-
-    angle = cube_red.addChild('Articulation')
-    # angle.addObject('MechanicalObject', name='dofs', template='Vec1', position=[[0.0]])
-
-    angle.addObject('MechanicalObject', name='dofs', template='Vec1', position=[[self.getData('angleIn')]],rest_position=self.getData('angleIn').getLinkPath())
-
-    angle.addObject('RestShapeSpringsForceField', points=0, stiffness=1e7)
-
-
-    articulationCenter = angle.addChild('ArticulationCenter')
-    articulationCenter.addObject('ArticulationCenter', parentIndex=0, childIndex=0, posOnParent=[0, 0., 2.8], posOnChild=[0., 0., 0.])
-    
-    articulation = articulationCenter.addChild('Articulations')
-    articulation.addObject('Articulation', translation=False, rotation=True, rotationAxis=[1, 0, 0],articulationIndex=0)
-    
-    angle.addObject('ArticulatedHierarchyContainer', printLog=False)
-
-
-    cube_blue = Cube(angle, name="Blue", totalMass=500, color=[0., 0., 1., 1.], translation=[-0.75, 5.5, 0.0], rotation=[45.0, 0.0, 20.0], uniformScale=0.8, isAStaticObject=False)
-
-    cube_blue.addObject('ArticulatedSystemMapping', input1="@../dofs", input2="@../../mstate", output="@./")
-
-
-
-
-
+    rootNode.addObject(CubeController(node=rootNode))
 
     Floor(scene.Modelling, translation=[0.0, -3.0, 0.0], rotation=[15.0, 0.0, 0.0], uniformScale=0.25, isAStaticObject=True)
 
