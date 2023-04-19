d6905dd8eaf15d7deb0bade72d5ccd14bf9f4eb1
Max Polzin <polzin.max@gmail.com>
2023-04-19 16:36:21 +0200

    Big success. Works.

--- two_cubes.py
+++ two_cubes.py
@@ -34,7 +34,7 @@ class CubeBot(Sofa.Prefab):
 
     def init(self):
 
-        cube_red = Cube(self, totalMass=500, name="Red", color=[1., 0., 0., 1.], translation=[1.25, 5.0, 0.0], rotation=[0.0, 90.0, 0.0], uniformScale=1.0, isAStaticObject=False)    
+        cube_red = Cube(self, totalMass=500, name="Red", color=[1., 0., 0., 1.], translation=[1.25, 15.0, 0.0], rotation=[0.0, 90.0, 0.0], uniformScale=1.0, isAStaticObject=False)    
 
         angle = cube_red.addChild('Articulation')
         angle.addObject('MechanicalObject', name='dofs', template='Vec1', position=[[self.getData('angleIn')]], rest_position=self.getData('angleIn').getLinkPath())
@@ -42,15 +42,15 @@ class CubeBot(Sofa.Prefab):
 
 
         articulationCenter = angle.addChild('ArticulationCenter')
-        articulationCenter.addObject('ArticulationCenter', parentIndex=0, childIndex=0, posOnParent=[0, 0., 2.8], posOnChild=[0., 0., 0.])
+        articulationCenter.addObject('ArticulationCenter', posOnParent=[0, 0., 1.95], parentIndex=0, childIndex=0, posOnChild=[0., 0., 0.])
         
         articulation = articulationCenter.addChild('Articulations')
-        articulation.addObject('Articulation', translation=False, rotation=True, rotationAxis=[1, 0, 0],articulationIndex=0)
+        articulation.addObject('Articulation', translation=False, rotation=True, rotationAxis=[0, 0, 1],articulationIndex=0)
         
         angle.addObject('ArticulatedHierarchyContainer', printLog=False)
 
 
-        cube_blue = Cube(angle, name="Blue", totalMass=500, color=[0., 0., 1., 1.], translation=[-0.75, 5.5, 0.0], rotation=[45.0, 0.0, 20.0], uniformScale=0.8, isAStaticObject=False)
+        cube_blue = Cube(angle, name="Blue", totalMass=500, color=[0., 0., 1., 1.], uniformScale=0.9, isAStaticObject=False)
 
         cube_blue.addObject('ArticulatedSystemMapping', input1="@../dofs", input2="@../../mstate", output="@./")
 
@@ -99,6 +99,6 @@ def createScene(rootNode):
 
     rootNode.addObject(CubeController(node=rootNode))
 
-    Floor(scene.Modelling, translation=[0.0, -3.0, 0.0], rotation=[15.0, 0.0, 0.0], uniformScale=0.25, isAStaticObject=True)
+    Floor(scene.Modelling, translation=[0.0, -3.0, 0.0], rotation=[15.0, 0.0, 0.0], uniformScale=0.4, isAStaticObject=True)
 
     return rootNode
